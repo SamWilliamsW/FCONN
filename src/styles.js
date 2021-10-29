61d12b1d001af18b2fd2fcc87affdaf5bb1c985d
Shaun Kelly <shaun37k@gmail.com>
2021-10-28 21:58:53 -0400

    extend background color at bottom, get rid of white side gutters

--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
     background: {
-        backgroundColor: '#112e51'
+        backgroundColor: '#112e51',
+        maxWidth: '100%',
+        height: '100vh'
     }
 }));
