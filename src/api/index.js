578b39ea3a2720aa7c90ba5811c14845a98edffc
Lucas Dale <lucasdale99@gmail.com>
2021-11-18 16:40:55 -0500

    Trying again on index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' || 'https://campuscantina.vercel.app/'});
+const API = axios.create({ baseURL: 'http://localhost:5000' || 'https://campuscantina.vercel.app'});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
